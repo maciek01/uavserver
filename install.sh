a4ca220971a19a4996cad487e7f99aef6477f860
maciek01 <maciek@kolesnik.org>
2017-11-26 07:05:36 +0000

    dockerize

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 
+docker container rm pi-uavserver
 docker image rm uavserver
-
 docker build -t uavserver .
-
-docker run --name pi-uavserver --restart always -v ~/uavserver/log:/app/log:rw -d -p 9000:9000 uavserver
+docker run --name pi-uavserver --restart always -v ~/uavserver/log:/app/log:rw -d -p 9091:9000 uavserver
 
 
 
