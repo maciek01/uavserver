87d481cec68aef9cbba5c032ad627d93751244e9
maciek01 <maciek@kolesnik.org>
2025-02-13 17:07:57 -0500

    get adsb data

--- bin/opensky-pump.py
+++ bin/opensky-pump.py
@@ -4,7 +4,7 @@ import traceback
 import time
 import requests
 #import json
-from opensky_api import OpenSkyApi
+#from opensky_api import OpenSkyApi
 
 
 
@@ -13,12 +13,17 @@ ONE_MINUTE_IN_DEC = 0.016667
 RANGE_IN_NM = 10 #check box of 10 nautical miles around the UAV
 
 
+#uavserver endpoints
 getHeartbeatsUrl = 'http://home.kolesnik.org:9091/uavserver/v1/heartbeats'
-
 postAdsbUrl = 'http://home.kolesnik.org:9091/uavserver/v1/adsb'
 
+#ADSB providers endpoints
 
-getAdsbsUrl = 'https://maciek:maciek@opensky-network.org/api/states/all?'
+#airplanes live
+getAdsbsUrlAPL = 'https://api.airplanes.live/v2/point/'
+
+#opensky
+getAdsbsUrlOS = 'https://maciek:maciek@opensky-network.org/api/states/all?'
 
 #osApi = OpenSkyApi("", "")
 #osApi = OpenSkyApi()
@@ -45,7 +50,7 @@ def processAdsb(unitId, adsbs):
 
 	return resp.ok
 
-def processHeartbeat(heartbeat):
+def processHeartbeatAPL(heartbeat):
 	
 	beat = heartbeat["heartbeat"]
 
@@ -56,20 +61,14 @@ def processHeartbeat(heartbeat):
 
 	if lat is not None and lon is not None:
 
-		lamin = lat - ONE_MINUTE_IN_DEC * RANGE_IN_NM
-		lamax = lat + ONE_MINUTE_IN_DEC * RANGE_IN_NM
-		lomin = lon - ONE_MINUTE_IN_DEC * RANGE_IN_NM * 2 #longitude is 2 times shorter than latitude at mid-latitudes
-		lomax = lon + ONE_MINUTE_IN_DEC * RANGE_IN_NM * 2 #longitude is 2 times shorter than latitude at mid-latitudes
+		url = getAdsbsUrlAPL + str(lat) + '/' + str(lon) + '/' + str(2 * RANGE_IN_NM)
 
-
-		url = getAdsbsUrl + 'lamin=' + str(lamin) + '&lomin=' + str(lomin) + '&lamax=' + str(lamax) + '&lomax=' + str(lomax)
-
-		print(url)
+		#print(url)
 
 		try:
 			adsbs = requests.get(url, timeout=5)
 			if adsbs.status_code == 200 and adsbs.ok:
-				print(adsbs.json())
+				#print(adsbs.json())
 				processAdsb(unitId, adsbs.json())
 			else:
 				print("error from OpenSky ", adsbs.status_code)
@@ -92,6 +91,79 @@ def processHeartbeat(heartbeat):
 
 	return
 
+def processHeartbeatOSRest(heartbeat):
+	
+	beat = heartbeat["heartbeat"]
+
+	unitId = beat["unitId"] if beat is not None else None
+
+	lat = beat["gpsLat"] if beat is not None else None
+	lon = beat["gpsLon"] if beat is not None else None
+
+	if lat is not None and lon is not None:
+
+		lamin = lat - ONE_MINUTE_IN_DEC * RANGE_IN_NM
+		lamax = lat + ONE_MINUTE_IN_DEC * RANGE_IN_NM
+		lomin = lon - ONE_MINUTE_IN_DEC * RANGE_IN_NM * 2 #longitude is 2 times shorter than latitude at mid-latitudes
+		lomax = lon + ONE_MINUTE_IN_DEC * RANGE_IN_NM * 2 #longitude is 2 times shorter than latitude at mid-latitudes
+
+
+		url = getAdsbsUrlOS + 'lamin=' + str(lamin) + '&lomin=' + str(lomin) + '&lamax=' + str(lamax) + '&lomax=' + str(lomax)
+
+		print(url)
+
+		try:
+			adsbs = requests.get(url, timeout=5)
+			if adsbs.status_code == 200 and adsbs.ok:
+				print(adsbs.json())
+				processAdsb(unitId, adsbs.json())
+			else:
+				print("error from OpenSky ", adsbs.status_code)
+
+		except requests.RequestException:
+			traceback.print_exc()
+
+	return
+
+def processHeartbeatOSApi(heartbeat):
+	
+	beat = heartbeat["heartbeat"]
+
+	unitId = beat["unitId"] if beat is not None else None
+
+	lat = beat["gpsLat"] if beat is not None else None
+	lon = beat["gpsLon"] if beat is not None else None
+
+	if lat is not None and lon is not None:
+
+		lamin = lat - ONE_MINUTE_IN_DEC * RANGE_IN_NM
+		lamax = lat + ONE_MINUTE_IN_DEC * RANGE_IN_NM
+		lomin = lon - ONE_MINUTE_IN_DEC * RANGE_IN_NM * 2 #longitude is 2 times shorter than latitude at mid-latitudes
+		lomax = lon + ONE_MINUTE_IN_DEC * RANGE_IN_NM * 2 #longitude is 2 times shorter than latitude at mid-latitudes
+
+
+		url = getAdsbsUrl + 'lamin=' + str(lamin) + '&lomin=' + str(lomin) + '&lamax=' + str(lamax) + '&lomax=' + str(lomax)
+
+		print(url)
+
+		try:
+			#API WAY
+			#states = osApi.get_states(time_secs=0, icao24=None, bbox=(lamin, lamax, lomin, lomax))
+
+			#print(states)
+			#if states != None:
+				#for s in states.states:
+				#	print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
+
+				#print(states["states"])
+
+			#else:
+				#print("no sttes")
+			print()
+		except requests.RequestException:
+			traceback.print_exc()
+
+	return
 
 while True:
 
@@ -106,7 +178,7 @@ while True:
 			heartbeats = heartbeatsResp.json()
 
 			for beat in heartbeats["data"]["heartbeats"]:
-				processHeartbeat(beat)
+				processHeartbeatAPL(beat)
 
 
 
@@ -115,7 +187,7 @@ while True:
 		traceback.print_exc()
 
 
-	time.sleep(30)
+	time.sleep(5)
 
 
 
